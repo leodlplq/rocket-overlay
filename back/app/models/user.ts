8634b8782e0071512081f2d8ccdc4945806f1cd2
Léo Delplanque <delplanqueleomail@gmail.com>
2024-03-24 22:05:22 +0100

    feat: add field to users

--- back/app/models/user.ts
+++ back/app/models/user.ts
@@ -1,9 +1,9 @@
-import { DateTime } from 'luxon'
 import { withAuthFinder } from '@adonisjs/auth'
-import hash from '@adonisjs/core/services/hash'
-import { compose } from '@adonisjs/core/helpers'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
 import { DbAccessTokensProvider } from '@adonisjs/auth/access_tokens'
+import { compose } from '@adonisjs/core/helpers'
+import hash from '@adonisjs/core/services/hash'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { DateTime } from 'luxon'
 
 const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
   uids: ['email'],
@@ -15,7 +15,13 @@ export default class User extends compose(BaseModel, AuthFinder) {
   declare id: number
 
   @column()
-  declare fullName: string | null
+  declare firstname: string
+
+  @column()
+  declare lastname: string
+
+  @column()
+  declare username: string
 
   @column()
   declare email: string
